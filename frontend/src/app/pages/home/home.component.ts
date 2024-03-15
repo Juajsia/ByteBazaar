d1c8cd95f03ac9e2674bb1c86b440bb44ec767e1
cbisac <cortesbuitragoisac@gmail.com>
2024-03-14 22:30:22 -0500

    style(frontend): add styles home page

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, NavbarComponent],
+  imports: [CarouselComponent, NavbarComponent, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  arrow = faChevronRight
 }
