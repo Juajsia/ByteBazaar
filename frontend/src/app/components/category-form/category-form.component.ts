39f357e323e1e90e19a749073efc2a13af561e47
Juan Adams <juan_adams82212@elpoli.edu.co>
2024-04-01 23:01:17 -0500

    feat(frontend)
    add categoryform validators

--- frontend/src/app/components/category-form/category-form.component.ts
+++ frontend/src/app/components/category-form/category-form.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmark, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule, FormsModule, ReactiveFormsModule],
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.css'
 })
@@ -15,6 +16,11 @@ export class CategoryFormComponent {
   xIcon = faXmark
   diskIcon = faFloppyDisk
   pageTitle = 'Add New Category'
+
+  form = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required)
+  })
 
   constructor(private router: Router) {
 
